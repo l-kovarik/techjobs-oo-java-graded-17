f7b4a00f19ae55e0dbede548dd2bd1cbc0ebd739
lorikovarik80 <kovarik.lori@gmail.com>
2023-10-04 21:45:13 -0500

    Task 6 git autograder

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,12 @@
+package org.launchcode.techjobs.oo;
+
+public abstract class JobField {
+//    int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public JobField(int id, String value) {
+//        this.id = id;
+//        this.value = value;
+//    }
+}
